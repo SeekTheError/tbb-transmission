14784fe5bbeb6514aac48333938c47e6b84196fc
Rémi Bouchar <remi.bouchar@gmail.com>
2014-05-19 21:21:11 +0200

    add search option,usage, split function, add jshintrc

--- bin/tbb-transmission.js
+++ bin/tbb-transmission.js
@@ -1,12 +1,35 @@
-#!/usr/bin/env node
+//#!/usr/bin/env node
 'use strict';
 
-var downloadBest = require('../lib/index.js');
+var lib = require('../lib/index.js');
+var query = process.argv.splice(2);
 
-downloadBest(process.argv.splice(2).concat(''), function(err, torrentName) {
+if (query.length === 0) {
+  console.log('usage:\n  tbb-transmission [-s] the torrent name\n  -s:  only search for the torrent');
+  return;
+}
+
+if (query.indexOf('-s') === -1) {
+  lib.downloadBestTorrent(query, function(err, torrentName) {
+    if (err) {
+      return console.log('Error:', err);
+    }
+
+    console.log('Downloading', torrentName);
+  });
+
+  return;
+}
+
+// remove the -s parameter
+query.splice(query.indexOf('-s'),1);
+
+lib.getBestTorrent(query, function(err, magnet) {
   if (err) {
     return console.log('Error:', err);
   }
 
-  console.log('Downloading', torrentName);
+  // as ugly as it gets
+  var name = decodeURIComponent(magnet.split('&dn=')[1].split('&tr=')[0]);
+  console.log('Best torrent Name:', name);
 });
