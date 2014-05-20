e08aa9e04402b5075eae44e13c9a2d2ed162e74e
Rémi Bouchar <remi.bouchar@gmail.com>
2014-05-20 20:19:42 +0200

    add jscs, switch to optimist for agv parsing

--- bin/tbb-transmission.js
+++ bin/tbb-transmission.js
@@ -1,35 +1,37 @@
 #!/usr/bin/env node
 'use strict';
-
+var argv = require('optimist').argv;
 var lib = require('../lib/index.js');
-var query = process.argv.splice(2);
 
-if (query.length === 0) {
-  console.log('usage:\n  tbb-transmission [-s] the torrent name\n  -s:  only search for the torrent');
-  return;
-}
-
-if (query.indexOf('-s') === -1) {
-  lib.downloadBestTorrent(query, function(err, torrentName) {
-    if (err) {
-      return console.log('Error:', err);
-    }
-
-    console.log('Downloading', torrentName);
-  });
-
-  return;
-}
-
-// remove the -s parameter
-query.splice(query.indexOf('-s'), 1);
-
-lib.getBestTorrent(query, function(err, magnet) {
-  if (err) {
-    return console.log('Error:', err);
+(function() {
+  if (!argv._) {
+    console.log('usage:\n  tbb-transmission [-s] the torrent name\n  -s:  only search for the torrent');
+    return;
   }
 
-  // as ugly as it gets
-  var name = decodeURIComponent(magnet.split('&dn=')[1].split('&tr=')[0]);
-  console.log('Best torrent Name:', name);
-});
+  var query = argv._;
+
+  if (!argv.s) {
+    lib.downloadBestTorrent(query, function(err, torrentName) {
+      if (err) {
+        console.log('Error:', err);
+        return;
+      }
+
+      console.log('Downloading', torrentName);
+    });
+
+    return;
+  }
+
+  lib.getBestTorrent(query, function(err, magnet) {
+    if (err) {
+      console.log('Error:', err);
+      return;
+    }
+
+    // as ugly as it gets
+    var name = decodeURIComponent(magnet.split('&dn=')[1].split('&tr=')[0]);
+    console.log('Best torrent Name:', name);
+  });
+})();
